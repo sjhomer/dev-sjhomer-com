66960e211a7b42715811cf32aa05642590d2b50a
SJ Homer <dev@sjhomer.com>
2022-04-30 00:42:51 -0400

    feat(home): Continuing to flush theme/elements
    * Adjusting the footer to stay present briefly as the user scrolls from the initial page height, but not enough to be bothersome

--- web/components/layout/Footer/useFooterState.ts
+++ web/components/layout/Footer/useFooterState.ts
@@ -11,10 +11,11 @@ export default function useFooterState() {
    * On scroll, hide the footer as the user scrolls down, but show it again on scroll up.
    */
   const hideShowFooterOnScroll = React.useCallback(() => {
-    const {scrollY: currentScrollPos} = safeWindow
-    const maxScroll = document.body.scrollHeight - safeWindow.innerHeight
+    const {scrollY: currentScrollPos, innerHeight} = safeWindow
+    const maxScroll = document.body.scrollHeight - innerHeight
     // If we scroll down, hide the footer, unless we're near the end of the page. On scroll up, show it again.
-    const display = currentScrollPos < state.previousScroll || currentScrollPos >= maxScroll - 300
+    const display = currentScrollPos < innerHeight || currentScrollPos < state.previousScroll ||
+      currentScrollPos >= maxScroll - 300
 
     setState({
       display,
